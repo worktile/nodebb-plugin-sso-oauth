aae2c7c4d6406e02e10495241d7d58e00608ef7e
zhaoli <zhaoli@worktile.com>
2021-03-05 19:35:49 +0800

    fix: x

--- library.js
+++ library.js
@@ -112,7 +112,6 @@
 				// OAuth 2 options
 				opts = constants.oauth2;
 				opts.callbackURL = nconf.get('url') + '/auth/' + constants.name + '/callback';
-				opts.scope = "profile";
 
 				passportOAuth.Strategy.prototype.userProfile = function (accessToken, done) {
 					this._oauth2.get(constants.userRoute, accessToken, function (err, body/* , res */) {
@@ -154,7 +153,7 @@
 				url: '/auth/' + constants.name,
 				callbackURL: '/auth/' + constants.name + '/callback',
 				icon: 'fa-check-square',
-				scope: (constants.scope || '').split(','),
+				scope: (constants.scope || 'profile').split(','),
 			});
 
 			callback(null, strategies);
